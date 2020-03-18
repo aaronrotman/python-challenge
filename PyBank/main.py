d71296dad47f346e7fe41bc49aa22300c05f53ed
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-03-18 13:34:11 -0700

    Create a list for each row in the csv file and append that list to another list

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,6 +9,9 @@ import csv
 #Assign the file path to a variable
 csv_path = os.path.join("..", "local", "budget_data.csv")
 
+#Create an empty list that will store each row of the csv file as its own list within this list
+csv_list =[]
+
 with open(csv_path) as csv_file:
 
     #Select the delimiter and choose a variable to store that selection
@@ -16,6 +19,18 @@ with open(csv_path) as csv_file:
 
     #Read each row in the csv file
     for row in csv_reader:
+        
+        #Create an empty list that will temporarily store the current row as a list
+        temp_list = []
+        
+        #Append the current row to the temporary list
+        temp_list.append(row)
+        
+        #Append the temporary list to csv_list
+        csv_list.append(temp_list)
+        
         #Print the current row
-        print(row)
-  
+        #print(row)
+        
+    #for list in csv_list:
+    #    print(list)
