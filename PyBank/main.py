fe4980e5ff8e776073f5057a00b384f9d79d68a7
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-03-18 13:54:40 -0700

    Fix logic error: remove temp_list, rows are now appended directly to csv_list; find first and last date

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,20 +17,22 @@ with open(csv_path) as csv_file:
     #Select the delimiter and choose a variable to store that selection
     csv_reader = csv.reader(csv_file, delimiter = ",")
 
+    csv_header = next(csv_reader)
+    print(csv_header)
+
     #Read each row in the csv file
     for row in csv_reader:
         
-        #Create an empty list that will temporarily store the current row as a list
-        temp_list = []
-        
-        #Append the current row to the temporary list
-        temp_list.append(row)
-        
         #Append the temporary list to csv_list
-        csv_list.append(temp_list)
+        csv_list.append(row)
         
         #Print the current row
         #print(row)
-        
-    #for list in csv_list:
-    #    print(list)
+
+    for list in csv_list:
+        print(list)
+
+first_date = csv_list[0][0]
+last_date = csv_list [-1][0]
+print(f"First date: {first_date}")
+print(f"Last Date: {last_date}")
