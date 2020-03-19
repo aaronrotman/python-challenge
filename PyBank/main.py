fc6921a26e0ee8891f2013200ddbeed56f775bd1
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-03-18 22:19:41 -0700

    Rewrite the calculations for max_increase and max_decrease to calculate the proper values

--- PyBank/main.py
+++ PyBank/main.py
@@ -37,53 +37,11 @@ with open(csv_path) as csv_file:
     #Read each row in the csv file
     for row in csv_reader:
         
-        #Print the current row
-        #print(row)
-
-        #Append the temporary list to csv_list
+        #Change the current row 'Amount' type to integer
+        row[1] = int(row[1])
+  
         csv_list.append(row)
-
-    #Loop through each list in csv_list
-    for list in csv_list:
         
-        #print(list)
-        #print(f"Column 1 type: {type(list[0])}")
-        #print(f"Column 2 type: {type(list[1])}")
-
-        #Check if the current list's 'Amount' value is greater than max_increase
-        if int(list[1]) > int(max_increase['Amount']):
-            
-            #Set the max_increase 'Date' value to the current list's 'Date' value
-            max_increase['Date'] = list[0]
-            #print(max_increase['Date'])
-            
-            #Set the max_increase 'Amount' value equal to the current list's 'Amount' value
-            max_increase['Amount'] = int(list[1])
-            #print(max_increase['Amount'])
-
-        #Check if the current list's 'Amount value is less than max_increase
-        elif int(list[1]) < int(max_decrease['Amount']):
-            
-            #Set the max_decrease 'Date' value to the current list's 'Date' Value
-            max_decrease['Date'] = list[0]
-            #print(max_decrease['Date'])
-
-            #Set the max_decrease 'Amount' value to the current list's 'Amount' value
-            max_decrease['Amount'] = int(list[1])
-            #print(max_decrease['Amount'])
-        
-        elif int(list[1]) == max_increase['Amount']:
-            print("Uh oh! There are multiple values for max_increase")
-        
-        elif int(list[1]) == max_decrease['Amount']:
-            print("Uh oh! There are multiple values for max_decrease")
-
-    #Print the 'Date' and 'Amount' for the time period with greatest increase in profits
-    print(f"The greatest increase in profits was in {max_increase['Date']} with a profit of {max_increase['Amount']}")
-
-    #Print the 'Date' and 'Amount' for the time period with greatest decrease in profits
-    print(f"The greatest decrease in profits was in {max_decrease['Date']} with a loss of {max_decrease['Amount']}") 
-    
 #Store the first date in a variable
 first_date = csv_list[0][0]
 print(f"First date: {first_date}")
@@ -92,8 +50,54 @@ print(f"First date: {first_date}")
 last_date = csv_list [-1][0]
 print(f"Last Date: {last_date}")
 
+
 #Calculate the number of months included in the data set
+
+#Count the length of csv_list and assign that value to a variable
 data_months = len(csv_list)
 print(f"This analysis covers {data_months} months of data.")
 
+
+#Calculate the month with the greatest increase in profits relative to the previous month
+
+
+#Iterate through each list in csv_list
+for i in range(len(csv_list)):
+
+    #Store the current months profits in a temporary variable
+    current_profit = csv_list[i][1]
+    current_date = csv_list[i][0]
+    #Store the previous months profits in a temporary variable
+    previous_profit = csv_list[i-1][1]
+    profit_change = (current_profit - previous_profit)
+
+    #print(f"Current Date: {current_date} Current profit: {current_profit} Previous Profit: {previous_profit} Profit Change: {profit_change}")
+
+
+    #Check if the current month's change in profits is greater than the previous max increase 'Amount'
+    if profit_change > max_increase['Amount']:
+        
+        #Set the 'Date' value for max_increase equal to the current month's 'Date'
+        max_increase['Date'] = current_date
+        #print(max_increase['Date'])
+        
+        #Set the 'Amount' value for max_increase equal to the current months's change in profit
+        max_increase['Amount'] = profit_change
+        #print(max_increase['Amount'])
+
+    #Check if the current month's change in profits is less than the previous max_decrease 'Amount'
+    elif profit_change < max_decrease['Amount']:
+
+        #Set the 'Date' value for max_decrease equal to the current month's date
+        max_decrease['Date'] = current_date
+
+        #Set the 'Amount' value for max_decrease equal to the current month's change in profits
+        max_decrease['Amount'] = profit_change
+        
+    
+#Print the 'Date' and 'Amount' for the time period with greatest increase in profits
+print(f"The greatest increase in profits was in {max_increase['Date']} with a profit of {max_increase['Amount']}")
+
+#Print the 'Date' and 'Amount' for the time period with greatest decrease in profits
+print(f"The greatest decrease in profits was in {max_decrease['Date']} with a loss of {max_decrease['Amount']}") 
 
