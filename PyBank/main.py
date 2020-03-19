0c032229ff7df5bfbe31eb15ff267367654edf98
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-03-19 13:38:17 -0700

    Calculate mean change; change code that calculates current change per month to ignore the first month

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,6 +11,12 @@ csv_path = os.path.join("..", "local", "budget_data.csv")
 #Create an empty list that will store each row of the csv file as its own list within this list
 csv_list =[]
 
+#An empty list that will store the change in profit for each month as a list item
+change_list = []
+
+#Variabale to store the net profit or loss for the time period covered by this data
+net_profit = 0
+
 #Create a dictionary to store 'Date' and 'Amount' for the month with the greatest increase in profits
 max_increase = {
     #Set 'Amount' to start at 0
@@ -24,6 +30,7 @@ max_decrease = {
     'Amount': 0,
 }
 
+#Read the csv file and append each row of data as a list to a new list
 with open(csv_path) as csv_file:
 
     #Assign a variable to store the csv.reader object
@@ -35,15 +42,11 @@ with open(csv_path) as csv_file:
 
     #Read each row in the csv file
     for row in csv_reader:
-        
         #Change the current row 'Amount' type to integer
         row[1] = int(row[1])
         #Append the current month's data as a list to csv_list
         csv_list.append(row)
     
-#Variabale to store the net profit or loss for the time period covered by this data
-net_profit = 0
-
 #Calculate the number of months included in the data set
 #Count the length of csv_list and assign that value to a variable
 data_months = len(csv_list)
@@ -61,15 +64,21 @@ for i in range(len(csv_list)):
     #Store the current month's change in profits
     profit_change = (current_profit - previous_profit)
 
+    if i == 0:
+        pass
+
     #Check if the current month's change in profits is greater than the previous max increase 'Amount'
-    if profit_change > max_increase['Amount']:
+    elif profit_change > max_increase['Amount']:
         
         #Set the 'Date' value for max_increase equal to the current month's 'Date'
         max_increase['Date'] = current_date
        
         #Set the 'Amount' value for max_increase equal to the current months's change in profit
         max_increase['Amount'] = profit_change
-    
+
+        #Append the current month's change in profits to change_list
+        change_list.append(profit_change)
+
     #Check if the current month's change in profits is less than the previous max_decrease 'Amount'
     elif profit_change < max_decrease['Amount']:
 
@@ -78,9 +87,19 @@ for i in range(len(csv_list)):
 
         #Set the 'Amount' value for max_decrease equal to the current month's change in profits
         max_decrease['Amount'] = profit_change
+
+        #Append the current month's change in profits to change_list
+        change_list.append(profit_change)
     
-    #Add the current months profit to the net profit counter
+    elif profit_change < max_increase['Amount'] and profit_change > max_decrease['Amount']:
+        #Append the current month's change in profits to change_list
+        change_list.append(profit_change)
+    
+    #Add the current months profit value to the net profit counter
     net_profit += current_profit
+    
+
+    
 
 #Print out a summary of key metrics
 #Print the number of months that the data set covers.
@@ -94,4 +113,17 @@ print(f"The greatest increase in profits was in {max_increase['Date']} with a pr
 
 #Print the 'Date' and 'Amount' for the time period with greatest decrease in profits
 print(f"The greatest decrease in profits was in {max_decrease['Date']} with a loss of {max_decrease['Amount']}") 
+
+
+#Variable to store the sum of the monthly change in profits
+sum_change = 0
+num_change = len(change_list)
+print(len(change_list))
+for item in change_list:
+    current_change = item
+    sum_change += current_change
+
+mean_change = sum_change/num_change
+
+print(f"Average change: {mean_change}")
 
