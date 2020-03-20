7216e8b4c1d68050cded9cc02adedfd5c248e24d
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-03-20 12:20:22 -0700

    Read each row in the csv file and append the row to a new list

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,13 +7,32 @@ import csv
 #Store the file path
 poll_path  = os.path.join("..", "local", "election_data.csv")
 
+#list[index][key] += 1
+
+#Empty list to store a dictionary for each candidate who receives a vote
+candidate_list = []
+
+#Variable to store the total number of votes cast in the election
+total_votes = 0
+
 #Open the csv file
 with open(poll_path) as election_data:
-    
+
     #Initialize the csv reader as file_reader
     file_reader = csv.reader(election_data, delimiter = ",")
-
+    #Assign the first row to a variable as the header
+    header_row = next(file_reader)
+    #Print the header row
+    print(header_row)
+    
     #Read each row in the csv file
     for row in file_reader:
+        #Append the current row to a new list
+        candidate_list.append(row)
+
+
         
-        
+print(len(candidate_list))     
+print(candidate_list[0]) 
+print(candidate_list[-1])
+print(total_votes)
