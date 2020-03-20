b16ef09eec058ec7317d591b26c916209012352f
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-03-20 13:45:13 -0700

    Script now reads each row in csv, appends candidate's name to a list, adds unique candidate names to another

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,8 +9,11 @@ poll_path  = os.path.join("..", "local", "election_data.csv")
 
 #list[index][key] += 1
 
-#Empty list to store a dictionary for each candidate who receives a vote
+#Empty list to store the name of the candidate selected for each vote
 candidate_list = []
+
+#Empty list to store the name of each unique candidate who received a vote
+unique_list = []
 
 #Variable to store the total number of votes cast in the election
 total_votes = 0
@@ -23,16 +26,29 @@ with open(poll_path) as election_data:
     #Assign the first row to a variable as the header
     header_row = next(file_reader)
     #Print the header row
-    print(header_row)
+    #print(header_row)
     
     #Read each row in the csv file
     for row in file_reader:
-        #Append the current row to a new list
-        candidate_list.append(row)
-
-
+        #Assign the candidates name to a variable
+        candidate_name = row[2]
+        #print(candidate_name)
+        #print(row)
+        #print(candidate_list)
         
+        #Append the candidates name to a new list
+        #candidate_list += candidate_name
+        if candidate_name in unique_list:
+            candidate_list.append(candidate_name)
+            #print(f"Unique List: {unique_list}")
+        elif candidate_name not in unique_list:
+            #print(candidate_name)
+            candidate_list.append(candidate_name)
+            unique_list.append(candidate_name)
+            #print(unique_list)
+        
+print(unique_list)
 print(len(candidate_list))     
-print(candidate_list[0]) 
-print(candidate_list[-1])
-print(total_votes)
+#print(candidate_list[0]) 
+#print(candidate_list[-1])
+#print(total_votes)
